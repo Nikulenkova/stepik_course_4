0efc9d3342937d5f4df11e77e9bc0ff8fe6e7902
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2025-05-22 13:22:16 +0500

    Добавление проверки элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,3 +5,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (имя исключения):
+            return False
+        return True
