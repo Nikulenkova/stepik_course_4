976f7bc7305637aace71622f374283c30bad127b
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2025-05-26 20:02:00 +0500

    Add basket_page.py

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,7 +5,7 @@ from pages.locators import BasketPageLocators
 class BasketPage(BasePage):
 
     def should_be_empty_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket is not empty"
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Корзина не пуста"
 
     def should_be_text_basket_is_empty(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE), "Empty basket message is missing"
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE), "Сообщение о пустой корзине отсутствует"
