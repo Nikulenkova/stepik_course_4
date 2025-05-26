fbcd413e1aee6622f4ec2d76bf0abe85aeeb0e10
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2025-05-26 20:06:16 +0500

    Add product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,15 +11,15 @@ class ProductPage(BasePage):
     def should_be_success_message_with_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT).text
-        assert product_name == success_message, f"Expected product '{product_name}', but got '{success_message}'"
+        assert product_name == success_message, f"Ожидаемый продукт '{product_name}', но получил '{success_message}'"
 
     def should_be_success_message_with_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
-        assert product_price == basket_total, f"Expected price '{product_price}', but got '{basket_total}'"
+        assert product_price == basket_total, f"Ожидаемая цена '{product_price}', но получил '{basket_total}'"
 
     def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT), "Success message is displayed, but should not be"
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT), "Отображается сообщение об успешном завершении, но его не должно быть"
 
     def success_message_should_disappear(self):
-        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT), "Success message did not disappear as expected"
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT), "Сообщение об успешном завершении не исчезло, как ожидалось"
